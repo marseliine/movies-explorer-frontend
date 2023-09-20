4912127f5f0184c4e86acab326c7741a5d644972
marseliine <78792953+marseliine@users.noreply.github.com>
2023-09-20 12:10:41 +0300

    update src directory

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-// export * from './Main/Main';
-// export * from './Login/Login';
-// export * from './Movies/Movies';
-// export * from './SavedMovies/SavedMovies';
-// export * from './NotFoundPage/NotFoundPage';
-// export * from './Profile/Profile';
-// export * from './Register/Register';
--- src/utils/constants/routes-constants.js
+++
@@ -1,8 +0,0 @@
-
-const ROUTES_WITH_FOOTER = ['/', '/movies', '/saved-movies'];
-
-const ROUTES_WITH_HEADER = ['/', '/movies', '/saved-movies', '/profile'];
-
-const ROUTES_WITH_BLACK_HEADER = ['/movies', '/saved-movies', '/profile'];
-
-export { ROUTES_WITH_FOOTER, ROUTES_WITH_HEADER, ROUTES_WITH_BLACK_HEADER };
