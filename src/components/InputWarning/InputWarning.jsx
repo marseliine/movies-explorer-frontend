87c1d3faf7150d441dd20f534f7312278b891622
marseliine <78792953+marseliine@users.noreply.github.com>
2023-10-08 19:23:40 +0300

    Delete src directory

--- src/components/InputWarning/InputWarning.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './InputWarning.module.css';
-
-const InputWarning = () => (
-  <p className={styles['register__form-warning']}>Что-то пошло не так...</p>
-);
-
-export default InputWarning;
