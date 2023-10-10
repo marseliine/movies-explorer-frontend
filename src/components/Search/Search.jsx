e5f578c1a7b2c0d02061427a716e1b6de2887924
marseliine <78792953+marseliine@users.noreply.github.com>
2023-10-10 23:56:25 +0300

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,19 @@
 function Search() {
     return (
         <>
-            <div className="search__center">
-                <form className="search">
+            <section className="search__center">
+                <div className="search">
                     <div className="search__container">
                         <input className="search__input" type="text" placeholder="Фильм" required />
                         <button className="search__btn" type="button"></button>
                     </div>
-                </form>
-                <div className="search__toggle-container">
+                </div>
+                <form className="search__toggle-container">
                     <input className="search__checkbox" type="checkbox" id="short-film-toggle" />
                     <label className="search__toggle" htmlFor="short-film-toggle"></label>
                     <p className="search__short-film-text">Короткометражки</p>
-                </div>
-            </div>
+                </form>
+            </section>
         </>
     )
 }
