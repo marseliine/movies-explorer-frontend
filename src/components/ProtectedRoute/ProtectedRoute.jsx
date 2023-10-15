74c9825314f79c626944e0ead29d68e89517c4ac
marseliine <78792953+marseliine@users.noreply.github.com>
2023-10-15 21:04:10 +0300

    Update

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element: Component, ...props }) => {
   return (
-    props.loggedIn ? <Component {...props} /> : <Navigate to="/" />
+    props.loggedIn
   )
 }
 
