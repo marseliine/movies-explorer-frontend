e453f2397041234414de7f98e274b975304c40d8
marseliine <78792953+marseliine@users.noreply.github.com>
2023-11-01 01:27:10 +0300

    update

--- src/components/SpanError/SpanError.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './SpanError.css';
-
-function SpanError({ error }) {
-  return error ? <span className='form__item-error'>{error}</span> : null;
-}
-
-export default SpanError;
